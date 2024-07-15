480976dee2bba1ab510a58767e5bb850d6fb5772
pdlp271 <109448588+pdlp271@users.noreply.github.com>
2024-07-15 19:34:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -144,7 +144,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://vj-filter-bot-1-nscj.onrender.com")
+URL = environ.get("URL", "https://vj-filter-bot-2-s7eu.onrender.com")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
